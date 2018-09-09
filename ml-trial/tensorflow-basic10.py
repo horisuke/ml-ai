47329a290e8384af818344f7b3f9b953ed19cb8d
horisuke-h1 <hori0621@gmail.com>
2018-09-09 13:34:28 +0900

    commit tensorflow-basic10.py in ml-trial project newly.

---
+++ ml-trial/tensorflow-basic10.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+LOG_DIR = './logs'
+
+a = tf.constant(1, name='a')
+b = tf.constant(1, name='b')
+c = a + b
+
+graph = tf.get_default_graph()
+with tf.summary.FileWriter(LOG_DIR) as writer:
+    writer.add_graph(graph)
