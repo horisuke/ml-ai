94bcd77179aee9664dd1f4609a59b7dbb149614f
horisuke-h1 <hori0621@gmail.com>
2018-09-09 14:29:15 +0900

    commit tensorflow-basic11.py in ml-trial project newly.

---
+++ ml-trial/tensorflow-basic11.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+
+# define parameters as variables.
+x = tf.Variable(0., name='x')
+# define the function to be minimized by parameters.
+func = (x - 1) ** 2
+
+# set the learning-rate
+optimizer = tf.train.GradientDescentOptimizer(
+    learning_rate=0.1
+)
+
+# train_step is the operation to move the values of x.
+train_step = optimizer.minimize(func)
+
+# execute train_step repeatly.
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(20):
+        sess.run(train_step)
+    print('x = ', sess.run(x))
