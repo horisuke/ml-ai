ebe8602581ac9ad33fa415e742fa6f1622655497
horisuke <hori0621@gmail.com>
2019-03-09 22:07:48 +0900

    Add numpy import.

--- ml-trial/ml.py
+++ ml-trial/ml.py
@@ -1,7 +1,10 @@
 import math as ma
+import numpy as np
 
+x = np.arange(1,5)
 print("Start")
 print("Hello!")
-print("pi = " + str(ma.pi))
+print("pi = ", ma.pi)
+print('x =', x)
 print("End")
 print("End2")
